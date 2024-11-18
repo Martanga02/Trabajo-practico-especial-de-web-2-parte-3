691eaf5afe18512c4117ea171c2dc89d7728844a
Martanga02 <145231552+Martanga02@users.noreply.github.com>
2024-11-17 21:27:12 -0300

    Update config.php

--- TrabajoPracticoEspecialTercerEntrega/config.php
+++ TrabajoPracticoEspecialTercerEntrega/config.php
@@ -1,12 +1,6 @@
 <?php
 
-    // const MYSQL_USER = 'root';
-    // const MYSQL_PASS = '';
-    // const MYSQL_DB = 'almacen_';
-    // const MYSQL_HOST = 'localhost';
-    // const JWT_KEY = 'hola';
-    // const JWT_EXP = 3600; // 1hs
-    
+   
     define('MYSQL_USER','root');
     define('MYSQL_PASS','');
     define('MYSQL_DB','almacen_');
